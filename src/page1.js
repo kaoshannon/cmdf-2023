da1a1d893bece87c6679ef1340c4e3b73dbba927
Michelle Teh <michelleteh@dhcp-128-189-29-123.ubcsecure.wireless.ubc.ca>
2023-03-11 17:30:08 -0800

    added routers

---
+++ src/page1.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function page1() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        this is page 1
+        </p>
+        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+      </header>
+    </div>
+  );
+}
+
+export default page1;
---
+++ src/page2.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function page2() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        this is page 2
+        </p>
+        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+      </header>
+    </div>
+  );
+}
+
+export default page2;
