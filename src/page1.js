0c71b2bd82b9eabb7266c15887915383d883791f
Shannon Kao <kaoshannon10@gmail.com>
2023-03-11 18:12:35 -0800

    started front end

--- src/page1.js
+++ src/page1.js
@@ -2,13 +2,22 @@ import './App.css';
 
 function page1() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-        this is page 1
-        </p>
+    <div className="row">
+      <div className="sidebar">
+        <h2>Dear Hiring Manager...</h2>
+        <h3>💎 Dashboard</h3>
+        <h3>🔥 Copy & Paste</h3>
+        <h3>👉 Add info</h3>
+        <h3>🚀 View results</h3>
+      </div>
+
+      <div className="main">
+        <h1>
+        Step 1. Copy & Paste
+        </h1>
+
         {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
-      </header>
+      </div>
     </div>
   );
 }
--- src/page2.js
+++ src/page2.js
@@ -1,8 +1,36 @@
 import './App.css';
+import axios from 'axios';
 
 function page2() {
+    const hitAPI = () => {
+        const options = {
+          method: 'POST',
+          url: 'https://api.cohere.ai/v1/generate',
+          headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            authorization: 'Bearer oeSQtij6wRDr91pa34NznONl3zjShiQo1EUVTjcQ'
+          },
+          data: {
+            max_tokens: 300,
+            return_likelihoods: 'NONE',
+            truncate: 'END',
+            prompt: 'Once upon a time in a magical land called'
+          }
+        };
+    
+        axios
+          .request(options)
+          .then(function (response) {
+            console.log(response.data);
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      }
+
   return (
-    <div className="App">
+    <div className="Page 2">
       <header className="App-header">
         <p>
         this is page 2
