c9cc36471a88bc097e0acbbf43bc7a54263905a4
Michelle Teh <michelleteh@Michelles-MacBook-Pro-6.local>
2023-03-12 10:07:37 -0700

    generate prompt string concat

--- src/page2.js
+++ src/page2.js
@@ -22,6 +22,8 @@ function Page2(object) {
   const [questions, setQuestions] = useState("");
   const apiKey = process.env.API_KEY;
   const generateQs = () => {
+    const prompt_questions = 'Based on this resume:' + object.prop.prop.resume + '\n And based on this job description:' + object.prop.prop.resume + '\n Generate 5 smart questions to get to know me better in my cover letter--'
+    console.log(prompt_questions)
     const options = {
       method: "POST",
       url: "https://api.cohere.ai/v1/generate",
@@ -38,7 +40,7 @@ function Page2(object) {
         temperature: 0.3,
         p: 0.75,
         prompt:
-          "Based on this cover letter below:\nMy most relevant working experience that replicates a Product Manager is at my most recent role at Procter & Gamble as an IT Manager, where I created a process to help automatically validate contracts. The previous processes involved people manually checking multiple fields across different datasets, each validation required around 10-15 minutes per contract. For example, whether the submitter had the correct permission and whether the supplier was valid. Furthermore, there were multiple data entry errors because there was no template available. To solve this, I ideated from scratch the process improvements, technical features, and user journeys. I communicated, pitched my solution to product owners of the datasets that the team used, reached out to cloud architect directors, and obtained approval from the global application manager for Coupa to launch the application in production.This experience taught me essential skills in product management, such as how to develop user stories, manage a dev team, and how to ensure that scope creep does not happen throughout the product development life cycle. In this case, I learned how to communicate to the client when a feature was out of scope, which we could work on within our team’s next sprint and explain why certain features could or could not be built. Furthermore, as we are only a small team spread out globally, I was also a developer and built the tool utilizing Microsoft Azure Logic Apps. \n\nGenerate 5 smart questions to get more information about me for my cover letter--",
+          prompt_questions,
         // prompt: 'Based on this\nGenerate five guiding questions about my past experience for me to answer in my cover letter that aligns with what the job description is looking for--'
       },
     };
